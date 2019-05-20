34cde2420643f5d2a65253171c02e9fd2fe615ce
mcw519 <roger4808@outlook.com>
2019-05-20 06:01:36 +0000

    fix path

--- meng_scripts/train_gmm_multi.sh
+++ meng_scripts/train_gmm_multi.sh
@@ -7,14 +7,14 @@ exp=$3
 train_cmd=queue.pl
 numjobs=400
 
-#multi_txt/train_mono_multi.sh --boost-silence 1.25 --cmd $train_cmd --nj $numjobs ${train_all}.100k $lang $exp/mono
-multi_txt/align_si_multi.sh --boost-silence 1.25 --cmd $train_cmd --nj $numjobs $train_all $lang $exp/mono $exp/mono_ali
+meng_scripts/train_mono_multi.sh --boost-silence 1.25 --cmd $train_cmd --nj $numjobs ${train_all}.100k $lang $exp/mono || exit 1;
+meng_scripts/align_si_multi.sh --boost-silence 1.25 --cmd $train_cmd --nj $numjobs $train_all $lang $exp/mono $exp/mono_ali
 
-multi_txt/train_deltas_multi.sh --boost-silence 1.25 --cmd $train_cmd 2500 20000 $train_all $lang $exp/mono_ali $exp/tri1
-multi_txt/align_si_multi.sh --cmd $train_cmd --nj $numjobs $train_all $lang $exp/tri1 $exp/tri1_ali
+meng_scripts/train_deltas_multi.sh --boost-silence 1.25 --cmd $train_cmd 2500 20000 $train_all $lang $exp/mono_ali $exp/tri1
+meng_scripts/align_si_multi.sh --cmd $train_cmd --nj $numjobs $train_all $lang $exp/tri1 $exp/tri1_ali
 
-multi_txt/train_lda_mllt_multi.sh --cmd $train_cmd 2500 20000 $train_all $lang $exp/tri1_ali $exp/tri2
-multi_txt/align_si_multi.sh --cmd $train_cmd --nj $numjobs $train_all $lang $exp/tri2 $exp/tri2_ali
+meng_scripts/train_lda_mllt_multi.sh --cmd $train_cmd 2500 20000 $train_all $lang $exp/tri1_ali $exp/tri2
+meng_scripts/align_si_multi.sh --cmd $train_cmd --nj $numjobs $train_all $lang $exp/tri2 $exp/tri2_ali
 
-multi_txt/train_sat_multi.sh --cmd $train_cmd 3500 100000 $train_all $lang $exp/tri2_ali $exp/tri3
-multi_txt/align_fmllr_lats_multi.sh --cmd $train_cmd --nj $numjobs $train_all $lang $exp/tri3 $exp/tri3_ali
+meng_scripts/train_sat_multi.sh --cmd $train_cmd 3500 100000 $train_all $lang $exp/tri2_ali $exp/tri3
+meng_scripts/align_fmllr_lats_multi.sh --cmd $train_cmd --nj $numjobs $train_all $lang $exp/tri3 $exp/tri3_ali
