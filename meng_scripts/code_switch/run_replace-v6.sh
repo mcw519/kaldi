e5edb1d5187ebcae57cb9a308954e588fa1844d9
mcw519 <roger4808@outlook.com>
2019-07-10 08:52:53 +0800

    fix error

--- meng_scripts/code_switch/run_replace-v6.sh
+++ meng_scripts/code_switch/run_replace-v6.sh
@@ -2,7 +2,9 @@
 
 python L_rule.py $1 temp.L
 python N_rule.py temp.L temp.LN
-phthon M_rule.py temp.LN temp.LNM
+python M_rule.py temp.LN temp.LNM
 python R_rule.py temp.LNM temp.LNMR
 
 cat temp.LNMR | sed 's/ NULL//g' | apply_map.pl -f 2- replace_v6.txt | sort -u > new_lexicon.txt
+
+rm temp.*
